90c5f26650a20edb2556dfebf8b95800ddcf9515
veroight <joaomarques.96@hotmail.com>
2015-01-29 18:58:41 +0000

    add new files

---
+++ learn_the_basics/output1.c
@@ -0,0 +1,24 @@
+#include <stdio.h> //library, which give the ability to execute necessary functions(printf)
+
+int main(void){
+	
+	printf("Hello ");
+	printf("world!\n");
+	
+	/*
+		Output: 
+		Hello world!
+	*/
+	
+	printf("20 + 10\n");
+	printf("40 + a \n");
+	
+	/*
+		Output:
+		20 + 10
+		40 + a 
+		
+	*/
+	
+	
+}
