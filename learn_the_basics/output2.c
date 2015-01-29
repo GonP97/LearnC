fcb7bfadef8ce56a3a10efff84948f0248061365
veroight <joaomarques.96@hotmail.com>
2015-01-29 19:12:59 +0000

    output2

---
+++ learn_the_basics/output2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int a; //creates a variable. For numbers, we will usually use the type integer(int)
+	a = 20; //associates the number 20 with the variable a
+	printf("%d", a); // %d means decimal integer
+	
+	printf("\n"); //  \n means new line
+	/*
+		output:
+		20
+		
+	*/
+	
+	int b;
+	b = 5;
+	printf("%d", a+b);
+	printf("\n");
+	
+	/*
+		output:
+		25
+		
+	*/
+	
+	
+	
+}
